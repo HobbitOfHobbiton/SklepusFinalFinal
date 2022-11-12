3925eb1c159d9f769948661acf6a0584677347b7
Michał Minikowski <perconey@gmail.com>
2022-11-12 09:31:03 +0100

    timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI txtTimer;
+
+    private Single _currentTime = 20;
+
+    public static event Action OnTimerEnded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Counting());
+
+    }
+
+    public void StartTimer()
+    {
+        StartCoroutine(Counting());
+    }
+
+    private IEnumerator Counting()
+    {
+        if(_currentTime <= 0)
+        {
+            OnTimerEnded?.Invoke();
+            print("timer ended");
+            LeanTween.scale(gameObject, Vector3.zero, 0.5f)
+                .setEaseInSine();
+
+            yield break;
+        }
+
+        yield return new WaitForSeconds(1f);
+        _currentTime--;
+        txtTimer.text = ((Int32)_currentTime).ToString();
+
+        StartCoroutine(Counting());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
