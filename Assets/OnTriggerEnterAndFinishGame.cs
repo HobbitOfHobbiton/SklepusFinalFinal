0e7a6ad51727292a1df40306f5fa9a4a0d01128f
Sharpshark <maciej.jasielewicz@gmail.com>
2022-11-13 07:40:13 +0100

    Fixed Boss fight

--- Assets/OnTriggerEnterAndFinishGame.cs
+++ Assets/OnTriggerEnterAndFinishGame.cs
@@ -7,11 +7,14 @@ using UnityEngine;
 public class OnTriggerEnterAndFinishGame : MonoBehaviour
 {
     [SerializeField] private OpenEyes openEyes;
+    [SerializeField] private GameObject finalText;
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerController>() != null)
         {
             openEyes.StartClosingEyes();
+            finalText.gameObject.SetActive(true);
+
         }
     }
 }
