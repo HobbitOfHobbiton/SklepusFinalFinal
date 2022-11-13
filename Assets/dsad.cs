10b383f20fb4031998e614c6b84b79bb53893cfc
Sharpshark <maciej.jasielewicz@gmail.com>
2022-11-13 08:58:39 +0100

    fix

---
+++ Assets/dsad.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dsad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.Instance.s();
+        AudioManager.Instance.ss();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
